0e7f39499930090af9b1a49c82e5a7a5abca9be8
Prashant Bhandari <cu.17bcs3286@gmail.com>
2023-05-27 10:34:45 +0545

    add view logic for login,create and listing

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Client
+from .forms import ClientForm
 
-# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('client_listing')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'login.html')
+
+def create_client(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('client_listing')
+    else:
+        form = ClientForm()
+    return render(request, 'create_client.html', {'form': form})
+
+def client_listing(request):
+    clients = Client.objects.all()
+    return render(request, 'client_listing.html', {'clients': clients})
+    
