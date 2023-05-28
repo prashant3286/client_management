1e60b2fdde03a474b207170e0a3fbfcc191492c0
Prashant Bhandari <cu.17bcs3286@gmail.com>
2023-05-28 13:41:28 +0545

    add education options in models

--- clients/models.py
+++ clients/models.py
@@ -11,15 +11,21 @@ class Client(models.Model):
         ('phone', 'Phone'),
         ('none', 'None'),
     ]
+    EDUCATION_CHOICES=[
+        ('slc','SLC'),
+        ('+2','+2'),
+        ('Bachelor','Bachelor'),
+    ]
 
     name = models.CharField(max_length=255)
     profile_image = models.ImageField(upload_to='client_profile_images')
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    gender = models.CharField(max_length=50,choices=GENDER_CHOICES)
     phone = models.CharField(max_length=20)
-    email = models.EmailField()
+    email = models.EmailField(max_length=50)
     nationality = models.CharField(max_length=100)
     date_of_birth = models.DateField()
-    education_background = models.TextField()
+    education_background = models.CharField(max_length=50,choices=EDUCATION_CHOICES)
     preferred_contact = models.CharField(max_length=10, choices=CONTACT_CHOICES)
 
     # Additional fields or relationships can be added here
